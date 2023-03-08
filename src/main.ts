74851ce7802b770747e8a23681a5beccd1a27523
littlemoonstones <littlemoonstones@gmail.com>
2023-03-08 11:57:27 +0800

    add tailwind css

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import "./app.scss";
+import "./app.css";
 import App from "./App.svelte";
 
 const app = new App({
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
+// import { vitePreprocess } from '@sveltejs/kit/vite';
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
